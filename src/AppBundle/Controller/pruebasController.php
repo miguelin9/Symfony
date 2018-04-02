67c59b3dc63f8be72bccd36c2c2d6906f7d815e4
miguelin9 <axrismiguelin@gmail.com>
2018-04-02 11:58:50 +0200

    Recoger variables GET y POST

--- src/AppBundle/Controller/pruebasController.php
+++ src/AppBundle/Controller/pruebasController.php
@@ -14,9 +14,13 @@ class pruebasController extends Controller {
         // por nombre de la ruta
 //        return $this->redirect($this->generateUrl("HolaMundo"));
         // usando request (se pone un parametro para saber que redirecciona bien)
-        return $this->redirect($request->getBaseUrl()."/helloWorld?hola=true");
+//        return $this->redirect($request->getBaseUrl()."/helloWorld?hola=true");
         // sin usar request
 //        return $this->redirect($this->container->get("router")->getContext()->getBaseUrl()."/helloWorld?hola=true");
+        
+        var_dump($request->query->get("hola"));
+        var_dump($request->get("hola-post"));// se usa este más que el de arriba
+        die();
         
         // replace this example code with whatever you need
         return $this->render('AppBundle:pruebas:index.html.twig', [
