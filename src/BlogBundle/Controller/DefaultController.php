220fccedabc08e7d46e7400078362e0c7de242f8
miguelin9 <axrismiguelin@gmail.com>
2018-04-03 16:06:21 +0200

    Relaciones Many-To-One

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -8,6 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
+        
+        foreach ($entries as $entry) {
+            echo $entry->getTitle()."<br/>";
+            echo $entry->getCategory()->getName()."<br/>";
+            echo $entry->getUser()->getName()."<br/><hr/>";
+        }
+        
+        die();
+        
         return $this->render('BlogBundle:Default:index.html.twig');
     }
 }
