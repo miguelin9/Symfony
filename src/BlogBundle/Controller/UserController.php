04b1a7ee78cbef2edcfbaead9a754b6bb366e3a4
miguelin9 <axrismiguelin@gmail.com>
2018-04-04 11:09:00 +0200

    Formulario de registro, salta el error por la versión de symfony o php

--- src/BlogBundle/Controller/UserController.php
+++ src/BlogBundle/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BlogBundle\Entity\User;
+use BlogBundle\Form\UserType;
 
 class UserController extends Controller{
     public function loginAction(Request $request) {      
@@ -11,9 +13,38 @@ class UserController extends Controller{
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
         
+        // Formulario
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $user = new User();
+            $user->setName($form->get("name")->getData());
+            $user->setSurname($form->get("surname")->getData());
+            $user->setEmail($form->get("email")->getData());
+            $user->setPassword($form->get("password")->getData());
+            $user->setRole("ROLE_USER");
+            $user->setImagen(null);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $flush = $em->flush();
+            if ($flush == null) {
+                $status = "El usuario se ha creado correctamente.";
+            } else {
+                $status = "No te has registrado correctamente.";
+            }                       
+        } else {
+            $status = "No te has registrado correctamente.";
+        }
+        // Fin formulario
+        
         return $this->render("BlogBundle:User:login.html.twig", array(
             "error" => $error,
-            "lastUsername" => $lastUsername
+            "lastUsername" => $lastUsername,
+            "form" => $form->createView()
         ));
     }
 }
