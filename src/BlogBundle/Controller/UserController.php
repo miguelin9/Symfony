2027fe90fc54e644fff5078b44ed04f02bfeba02
miguelin9 <axrismiguelin@gmail.com>
2018-04-09 10:06:10 +0200

    Control de usuarios duplicados

--- src/BlogBundle/Controller/UserController.php
+++ src/BlogBundle/Controller/UserController.php
@@ -32,26 +32,34 @@ class UserController extends Controller
 
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
-                $user = new User();
-                $user->setName($form->get("name")->getData());
-                $user->setSurname($form->get("surname")->getData());
-                $user->setEmail($form->get("email")->getData());
-
-                $factory = $this->get('security.encoder_factory');
-                $encoder = $factory->getEncoder($user);
-                $password = $encoder->encodePassword($form->get('password')->getData(), $user->getSalt());
-
-                $user->setPassword($password);
-                $user->setRole("ROLE_USER");
-                $user->setImagen(null);
-
                 $em = $this->getDoctrine()->getManager();
-                $em->persist($user);
-                $flush = $em->flush();
-                if ($flush == null) {
-                    $status = "El usuario se ha creado correctamente.";
+                $user_repo = $em->getRepository('BlogBundle:User');
+                $user = $user_repo->findOneBy(array('email' => $form->get('email')->getData()));
+
+                if (count($user == 0)) {
+                    $user = new User();
+                    $user->setName($form->get("name")->getData());
+                    $user->setSurname($form->get("surname")->getData());
+                    $user->setEmail($form->get("email")->getData());
+
+                    $factory = $this->get('security.encoder_factory');
+                    $encoder = $factory->getEncoder($user);
+                    $password = $encoder->encodePassword($form->get('password')->getData(), $user->getSalt());
+
+                    $user->setPassword($password);
+                    $user->setRole("ROLE_USER");
+                    $user->setImagen(null);
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($user);
+                    $flush = $em->flush();
+                    if ($flush == null) {
+                        $status = "El usuario se ha creado correctamente.";
+                    } else {
+                        $status = "No te has registrado correctamente.";
+                    }
                 } else {
-                    $status = "No te has registrado correctamente.";
+                    $status = "El usuario ya existe.";
                 }
             } else {
                 $status = "No te has registrado correctamente.";
